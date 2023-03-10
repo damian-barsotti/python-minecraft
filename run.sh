af64dcddca666e90194bbd0b9f544c6918e8d6e0
BuildTools <damian@famaf.unc.edu.ar>
2023-03-10 17:23:29 -0300

    Add dirname to path

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ NCONTAINERS=$("$DIRNAME"/docker-compose.sh ps -q | wc -l)
 
 if [ $NCONTAINERS == 2 ]
 then
-./docker-compose.sh exec -it mc-python "$@"
+"$DIRNAME"/docker-compose.sh exec -it mc-python "$@"
 else
     echoerr Run "$DIRNAME"/start.sh before.
     exit 1
