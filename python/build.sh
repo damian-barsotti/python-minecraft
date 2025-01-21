64ea295002f505da901703368c65a073b0941e85
BuildTools <damian@famaf.unc.edu.ar>
2025-01-21 20:08:09 -0300

    Version number update

--- python/build.sh
+++ python/build.sh
@@ -2,4 +2,4 @@
 
 DIRNAME="${0%/*}"
 
-docker build --tag damian-barsotti/mc-python:1.0 -f "$DIRNAME/"Dockerfile --progress plain "$@" "$DIRNAME"
+docker build --tag damian-barsotti/mc-python:2.0 -f "$DIRNAME/"Dockerfile --progress plain "$@" "$DIRNAME"
